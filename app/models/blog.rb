bfbed76110e2572a083c9378cf1d51099359ed29
ed wassermann <vis0vis@gmail.com>
2013-04-28 17:21:31 +0200

    Add #new_post to Blog

---
+++ app/models/blog.rb
@@ -0,0 +1,29 @@
+class Blog
+
+  attr_reader :entries
+  attr_writer :post_source
+
+  def initialize
+    @entries = []
+  end
+
+  def title
+    "Watching paint dry"
+  end
+
+  def subtitle
+    "The trusted source for drying paint news & opinion"
+  end
+
+  def new_post
+    post_source.call.tap do |p|
+      p.blog = self
+    end
+  end
+
+  private 
+  
+  def post_source
+    @post_source ||= Post.public_method(:new)
+  end
+end
